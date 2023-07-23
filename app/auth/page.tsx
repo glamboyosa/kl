dc5ed6979fa1126d2b914b746ad5dca38a78c598
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-07-23 20:38:48 +0100

    feat: add higher remaining

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -15,7 +15,7 @@ export default function Login() {
     const body = JSON.stringify({
       apiId: env.UNKEY_API_ID,
       prefix: "glam",
-      remaining: 2,
+      remaining: 12,
     });
     const response = await fetch("https://api.unkey.dev/v1/keys", {
       method: "post",
