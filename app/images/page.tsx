8545a50efdc65e3fbed07120f93fc63406e8962b
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-07-23 23:17:20 +0100

    make it responsive-ish

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -2,7 +2,7 @@ import ImageContainer from "@/components/images/images-container";
 
 export default function Page() {
   return (
-    <main className="h-screen  overflow-y-scroll flex justify-center items-center gap-60">
+    <main className="h-screen  overflow-y-scroll flex flex-row justify-center items-center gap-60 md:flex-col">
       <ImageContainer />
     </main>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatContainer from "@/components/chat/chat-container";
 
 export default function Home() {
   return (
-    <main className="h-screen overflow-y-scroll flex justify-center items-center gap-10">
+    <main className="h-screen overflow-y-scroll flex flex-row justify-center items-center gap-10 md:flex-col">
       <ChatContainer />
     </main>
   );
