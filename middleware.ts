34097ccf5fdd52ab3114d7442e5d1e9d268a147e
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-07-23 20:20:35 +0100

    feat: add `middleware.ts` file to do auth stuff

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { env } from "./env.mjs";
+
+export async function middleware(request: NextRequest) {
+  const cookie = request.cookies.get("authCookie");
+
+  const url = new URL(request.url);
+
+  if (!cookie || cookie.value.length === 0) {
+    return NextResponse.redirect(`${url.origin}/auth`);
+  }
+
+  const response = NextResponse.next();
+
+  return response;
+}
+export const config = {
+  matcher: ["/"],
+};
